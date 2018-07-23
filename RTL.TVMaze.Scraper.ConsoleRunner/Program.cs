7fb938764386fa19cabb500c1cc9975e9837de8c
Aliaksandr Anufryieu <aliaksandr_anufryieu@epam.com>
2018-07-23 11:25:05 +0300

    add Console.Runner

---
+++ RTL.TVMaze.Scraper.ConsoleRunner/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RTL.TVMaze.Scraper.Application.Contract;
+using RTL.TVMaze.Scraper.Application.Dependencies.Contract;
+using RTL.TVMaze.Scraper.Application.Dependencies.SqlDataStorage;
+using RTL.TVMaze.Scraper.Application.Dependencies.TVMaze;
+using System;
+using System.Net.Http;
+
+namespace RTL.TVMaze.Scraper.ConsoleRunner
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var host = new HostBuilder()
+                .ConfigureAppConfiguration((hostContext, configApp) =>
+                {
+                    configApp.AddJsonFile("appsettings.json", optional: true);
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddTransient<HttpClient>();
+                    services.AddTransient<ITVMazeClient, TVMazeClient>();
+
+                    var connectionString = hostContext.Configuration.GetConnectionString("TVMazeDatabase");
+                    services.AddSingleton(serviceProvider => new TVMazeContextFactory(connectionString));
+                    services.AddTransient<IDataStorage, DataStorage>();
+                    services.AddTransient<IScraper, Scraper.Application.Scraper>();
+
+                    services.AddHostedService<ScrapTVMazeEvery5MinutesService>();
+                })
+                .Build();
+
+            host.Run();
+        }
+    }
+}
---
+++ RTL.TVMaze.Scraper.ConsoleRunner/ScrapTVMazeEvery5MinutesService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Hosting;
+using RTL.TVMaze.Scraper.Application.Contract;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RTL.TVMaze.Scraper.ConsoleRunner
+{
+    internal class ScrapTVMazeEvery5MinutesService : IHostedService, IDisposable
+    {
+        private Timer _timer;
+        private IScraper _scraper;
+
+        public ScrapTVMazeEvery5MinutesService(IScraper scraper)
+        {
+            _scraper = scraper;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _timer = new Timer(Scrap, null, TimeSpan.Zero, TimeSpan.FromMinutes(5));
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _timer?.Change(Timeout.Infinite, 0);
+
+            return Task.CompletedTask;
+        }
+
+        private void Scrap(object state)
+        {
+            _scraper.RunAsync().Wait();            
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+    }
+}
